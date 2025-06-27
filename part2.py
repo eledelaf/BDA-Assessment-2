e9fcc4e193546290bf088ea973a67c4cd84d3e74
eledelaf <eledelaf@ucm.es>
2025-06-27 15:50:53 +0100

    For some reason i have 13 rows in the data set but i used 10 links

--- part2.py
+++ part2.py
@@ -18,4 +18,13 @@ if __name__ == "__main__":
             df = pd.concat([df, pd.DataFrame(records)], ignore_index=True)
     
     print(df.head())
+
+    # Extract the needed fields:
+    l_fields = ["id", "title", "uploader", "artist", "tags", 
+                "duration_seconds", "upload_date", "view_count", 
+                "like_count", "year_uploaded", "tag_count"]
+    df = df[l_fields]
+    print(df.head())
+    print(df.shape)
+    print(df["title"])
     df.to_csv("combined_data.csv", index = False)
