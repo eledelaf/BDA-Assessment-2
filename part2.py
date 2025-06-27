f25be100aeacde092673e4b92e335336c25a6a49
eledelaf <eledelaf@ucm.es>
2025-06-27 15:35:41 +0100

    P2 open json files

--- part2.py
+++ part2.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import glob 
 import json
+from collections import Counter
 
-# Read the .json metadata from the audio_output directory 
+if __name__ == "__main__":
+    
+    # Read the .json metadata from the audio_output directory 
+    with open("/Users/elenadelafuente/Desktop/MASTER/2 trimestre/Big Data/Assesments/BDA Capstone project/audio_output/Daft Punk - Giorgio By Moroder.json") as file:
+        data = json.load(file)
+    
+    print(data)
