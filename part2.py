f7322c433c20ccf74e02e42096cc037bc867961a
eledelaf <eledelaf@ucm.es>
2025-06-27 15:43:08 +0100

    Download the json into a csv

--- part2.py
+++ part2.py
@@ -4,9 +4,18 @@ import json
 from collections import Counter
 
 if __name__ == "__main__":
-    
+    PATH = "/Users/elenadelafuente/Desktop/MASTER/2 trimestre/Big Data/Assesments/BDA Capstone project/audio_output"
+    # This code is on the assessment codebook 
+    json_files = glob.glob("/Users/elenadelafuente/Desktop/MASTER/2 trimestre/Big Data/Assesments/BDA Capstone project/audio_output/*.json")
+
     # Read the .json metadata from the audio_output directory 
-    with open("/Users/elenadelafuente/Desktop/MASTER/2 trimestre/Big Data/Assesments/BDA Capstone project/audio_output/Daft Punk - Giorgio By Moroder.json") as file:
-        data = json.load(file)
+    df = pd.DataFrame()
+
+    for file in json_files:
+        with open(file, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            records = data if isinstance(data, list) else [data]
+            df = pd.concat([df, pd.DataFrame(records)], ignore_index=True)
     
-    print(data)
+    print(df.head())
+    df.to_csv("combined_data.csv", index = False)
